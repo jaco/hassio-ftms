7e63e97fdde925f739a494b745e4a1a0fe557e7f
jaco <jaco@i-chat.pl>
2025-01-22 11:22:59 +0100

    Update config_flow.py

--- custom_components/ftms/config_flow.py
+++ custom_components/ftms/config_flow.py
@@ -244,7 +244,6 @@ class FTMSConfigFlow(ConfigFlow, domain=DOMAIN):
             else self._ftms.supported_properties
         )
 
-        _LOGGER.debug(f"Device Information: {self._ftms.device_info}")
         _LOGGER.debug(f"Machine type: {self._ftms.machine_type!r}")
         _LOGGER.debug(f"Available sensors: {self._ftms.available_properties}")
         _LOGGER.debug(f"Supported settings: {self._ftms.supported_settings}")
